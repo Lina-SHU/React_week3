5f0acf361d90b315baed41a3a00238ace60f525a
Lina Chen <sakura0477@gmail.com>
2023-08-26 19:31:23 +0800

    remove count state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import Form from './Form'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Form />
